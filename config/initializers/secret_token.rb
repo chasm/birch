0334d3e3b4eef0829c44ad4893f5e0082917725b
chasm <charles.munat@gmail.com>
2013-07-15 11:19:28 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birch::Application.config.secret_key_base = 'aa577446c083fc25e3c8493943c59466529b140be6bee050a92293d834f79015423e548b8a8ba34cb8bec925ae0a722a77ad98e9fd01d040808f0b23b29dfe52'
